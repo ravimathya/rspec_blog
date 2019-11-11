6dd0591347111d82e6a92ba63628b0e97c19ae85
ravimathya <ravimathya.gmail.com>
2019-11-11 12:00:13 +0545

    form for new post is created and test is also written

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -20,4 +20,12 @@ RSpec.describe PostsController, type: :controller do
 
     end
 
+    describe "GET #new" do
+        it "renders the new template" do
+            get :new
+            expect(response).to render_template :new
+
+        end
+    end
+
 end
